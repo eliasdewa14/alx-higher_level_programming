a33f11d7e6e8056bb2aef465be56c16ef6a1028a
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-20 17:37:23 +0000

    Bugs for pycodestyle validation are fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0:
-  last_digit *= -1
+    last_digit *= -1
 
 if last_digit == 0:
-  str = "0"
+    str = "0"
 elif last_digit <= 5:
-  str = "less than 6 and not 0"
+    str = "less than 6 and not 0"
 else:
-  str = "greater than 5"
+    str = "greater than 5"
 print("Last digit of {} is {} and is {}".format(number, last_digit, str))
