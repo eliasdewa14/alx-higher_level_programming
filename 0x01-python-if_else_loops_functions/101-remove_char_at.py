08c30bbe7257c072c287dfec4d51b0cfa80c8e52
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-20 18:57:06 +0000

    Bugs are fixed for task 6 and 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,11 +2,11 @@
 def remove_char_at(str, n):
     new_str = ""
     for i in str:
-      if 0 <= n <= len(str):
-        if i == str[n]:
-          continue
+        if 0 <= n <= len(str):
+            if i == str[n]:
+                continue
+            else:
+                new_str += i
         else:
-          new_str += i
-      else:
-        new_str += i
-    return new_str
+            new_str += i
+        return new_str
