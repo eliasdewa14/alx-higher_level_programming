3fdc040c48e0d87ba9d96751fef41dcf353a85c5
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-20 17:05:58 +0000

    Mandatory tasks for 0x01-python

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+  new_str = ""
+  for ch in str:
+      if ord(ch) >= 97 and ord(ch) <= 123:
+          x = ord(ch) - 32
+          y = chr(x)
+          new_str += y
+      else:
+        new_str += ch
+  print(new_str)
