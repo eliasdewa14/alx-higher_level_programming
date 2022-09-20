4eb0a60eea53ced052ce834e0089c0bbef3750ed
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-20 18:43:21 +0000

    advanced tasks for 0x01-python

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_str = ""
+    for i in str:
+      if 0 <= n <= len(str):
+        if i == str[n]:
+          continue
+        else:
+          new_str += i
+      else:
+        new_str += i
+    return new_str
