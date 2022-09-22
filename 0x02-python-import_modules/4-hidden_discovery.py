f4474587d036b9554ef02198ebbf9ffaa36054e5
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-22 19:13:21 +0000

    Bugs are fixed for task 4 and 102

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 import sys
 import hidden_4
 
 if __name__ == "__main__":
     for name in dir(hidden_4):
-        if name[:1] != "_":
+        if name[:2] != "__":
             print(name)
