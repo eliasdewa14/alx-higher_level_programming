c6ca3387bebb07d96e25a6638ed95d5b233b5f3c
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-22 14:30:17 +0000

    Bugs are fixed for task 2, 3 and 5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-n = len(sys.argv)
-if n <= 1:
-    print("{} arguments.".format(n - 1))
-elif n == 2:
-    print("{} argument:".format(n - 1))
-    for i in range(1, n):
-        print("{}: {}".format(i, sys.argv[i]))
-else:
-    print("{} arguments:".format(n - 1))
-    for i in range(1, n):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+
+    n = len(sys.argv)
+    if n <= 1:
+        print("{} arguments.".format(n - 1))
+    elif n == 2:
+        print("{} argument:".format(n - 1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("{} arguments:".format(n - 1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-n = len(sys.argv)
-sum = 0
-for i in range(1, n):
-     sum += int(sys.argv[i])
-print("{}".format(sum))
+if __name__ == "__main__":
+    n = len(sys.argv)
+    sum = 0
+    for i in range(1, n):
+        sum += int(sys.argv[i])
+    print("{}".format(sum))
