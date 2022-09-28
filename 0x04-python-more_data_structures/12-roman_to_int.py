063d80a24a3af7deaac2d8f5765d8b2fd78cefaf
Elias-Dewa <elias.dewa19@gmail.com>
2022-09-28 18:18:22 +0000

    Bug is fixed for task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-def value(rom):
-    if (rom == 'I'):
-        return 1
-    if (rom == 'V'):
-        return 5
-    if (rom == 'X'):
-        return 10
-    if (rom == 'L'):
-        return 50
-    if (rom == 'C'):
-        return 100
-    if (rom == 'D'):
-        return 500
-    if (rom == 'M'):
-        return 1000
-    return -1
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string == None:
         return 0
+    value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     ans = 0
     i = 0
     while (i < len(roman_string)):
-        str1 = value(roman_string[i])
+        str1 = value[roman_string[i]]
         if (i + 1 < len(roman_string)):
-            str2 = value(roman_string[i + 1])
+            str2 = value[roman_string[i + 1]]
             if (str1 >= str2):
                 ans = ans + str1
                 i = i + 1
