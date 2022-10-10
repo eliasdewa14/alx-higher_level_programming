5f21b7901fb93629affda8565cbc0b93f4c32797
Elias-Dewa <elias.dewa19@gmail.com>
2022-10-10 12:24:36 +0000

    mandatory tasks for 0x05-python

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    my_new_list = []
+    for i in range(list_length):
+        try:
+            item = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            item = 0
+        except TypeError:
+            print("wrong type")
+            item = 0
+        except IndexError:
+            print("out of range")
+            item = 0
+        my_new_list.append(item)
+    return (my_new_list)
