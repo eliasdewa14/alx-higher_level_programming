2cd111cba1afd3eaa55817baf5cccc90f038c212
Elias-Dewa <elias.dewa19@gmail.com>
2022-10-11 22:14:47 +0000

    task 1 0x06-python

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
-
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-
 
     def __init__(self, size):
         """Initialize a new Square.
@@ -14,5 +11,4 @@ class Square:
         Args:
             size (int): The size of the new square.
 	"""
-
         self.__size = size
