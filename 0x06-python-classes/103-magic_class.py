fb795137eb626898a0bd3c871a50286fb1ec08ba
Elias-Dewa <elias.dewa19@gmail.com>
2022-10-12 15:11:43 +0000

    Bugs are fixed for task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -14,7 +14,7 @@ class MagicClass:
             radius (float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
-        if (type(radius) != int) and (type(radius) != float):
+        if (type(radius) is not int) and (type(radius) is not float):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
