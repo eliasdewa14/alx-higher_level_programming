ae4845201d75d0058a779f740dcdf7b8d90deec5
Elias-Dewa <elias.dewa19@gmail.com>
2022-10-12 08:23:44 +0000

    Pycodestyle validation error is fixed fot task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,7 +38,7 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         if ((type(value[0]) != int) or (type(value[1]) != int)):
             raise TypeError('position must be a tuple of 2 positive integers')
-        if ((value[0] < 0) or (value[1] < 0))):
+        if ((value[0] < 0) or (value[1] < 0)):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
