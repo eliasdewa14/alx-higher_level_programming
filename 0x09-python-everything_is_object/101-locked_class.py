e109446e9ab73530c9e1c8bbc19ed06474506ec5
Elias-Dewa <elias.dewa19@gmail.com>
2022-10-18 20:40:28 +0000

    mandatory and advanced tasks for 0x09-python

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines LockedClass class"""
+
+
+class LockedClass():
+    """Representation of class
+
+    Args:
+        first_name: name
+    """
+    __slots__ = (first_name)
