85b19dfb89e57d5543a9284d4e9b2eea4c216e6b
Elias-Dewa <elias.dewa19@gmail.com>
2022-10-25 21:09:54 +0000

    Bug is fixed for  task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-""" function that writes an Object to a text file, using a JSON"""
-
+"""function that writes an Object to a text file, using a JSON"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file"""
-  
     with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
