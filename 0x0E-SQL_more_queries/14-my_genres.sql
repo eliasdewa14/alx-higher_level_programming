4022eee0f929c3c670a0be3337e2bea500e0c4bc
Elias Dewa Ahmed <54449260+Elias-Dewa@users.noreply.github.com>
2022-11-30 13:27:35 -0800

    Bugs are fixed

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,8 +3,8 @@
 SELECT tv_genres.name
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_shows_genres.show_id
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
-ON tv_shows_genres.genre_id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,7 +3,7 @@
 SELECT tv_genres.name, tv_shows.title
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
-ON tv_shows.id = tv_shows_genres.show_id
+ON tv_shows.id = tv_show_genres.show_id
 LEFT OUTER JOIN tv_genres
-ON tv_shows_genres.genre_id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_genres.name, tv_shows.title;
