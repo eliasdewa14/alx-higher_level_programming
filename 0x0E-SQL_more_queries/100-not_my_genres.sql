9060bdbc367e21d371ba63103400907d305f40c6
Elias Dewa Ahmed <54449260+Elias-Dewa@users.noreply.github.com>
2022-12-01 10:21:57 -0800

    Bugs are fixed for advanced tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,7 +7,7 @@
 SELECT DISTINCT tv_genres.name
 FROM tv_genres
 WHERE tv_genres.name NOT IN (
-    SELECT tv_shows
+    SELECT tv_genres.name
     FROM tv_shows
     INNER JOIN tv_show_genres
     ON tv_show_genres.show_id = tv_shows.show_id
--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -10,9 +10,9 @@ WHERE tv_shows.title NOT IN (
     SELECT tv_shows.title
     FROM tv_shows
     INNER JOIN tv_show_genres
-    ON tv_show.id = tv_shows_genres.show_id
+    ON tv_shows.id = tv_show_genres.show_id
     INNER JOIN tv_genres
     ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_genres.name = "Cmedy"
+    WHERE tv_genres.name = "Comedy"
 )
 ORDER BY tv_shows.title;
