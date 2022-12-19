4bec838a4427867be0ee98f67a25b360e4031934
Elias Dewa Ahmed <54449260+Elias-Dewa@users.noreply.github.com>
2022-12-19 13:12:07 -0800

    JavaScript_warm up: advanced tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length > 3) {
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
   const sorted = process.argv.sort();
   const reverse = sorted.reverse();
   console.log(reverse[1]);
-} else {
-  console.log(0);
 }
