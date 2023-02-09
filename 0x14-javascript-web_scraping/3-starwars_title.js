55d2e55cee2deab149503d0f22c596dfe91bb461
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-09 11:04:54 -0800

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,6 @@ const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
