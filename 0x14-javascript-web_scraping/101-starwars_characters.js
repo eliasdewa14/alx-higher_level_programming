83ac10acc0279614a86f0eb3c0e286bc18db1881
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-09 11:22:27 -0800

    Advanced tasks

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi.dev/api/films/' + process.argv[2];
+let listChars = [];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  characters = JSON.parse(body).characters;
+  getCharacters(0);
+});
+
+const getCharacters = (count) => {
+  if (count === characters.length) {
+    return;
+  }
+
+  request(listChars[count], (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(JSON.parse(body).name);
+    getCharacters(count + 1);
+  });
+};
+
