c174d15ebec85bc1b05890f6a8044aff79f24f89
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-10 12:37:00 -0800

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
 
-
 const request = require('request');
 
 request.get(process.argv[2], { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
+  if (!error) {
+    const completedTask = {};
+    body.forEach((todo) => {
+      if (todo.completed) {
+        if (!completedTask[todo.userId]) {
+          completedTask[todo.userId] = 1;
+        } else {
+          completedTask[todo.userId] += 1;
+        }
+      }
+    });
+    console.log(completedTask);
   }
-
 });
